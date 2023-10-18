e414a1310ad19339645527afd16f1f28beaefac4
Chester Lee Coloma <cleecoloma@gmail.com>
2023-10-18 11:13:28 -0700

    added roll property to the response

--- index.js
+++ index.js
@@ -3,17 +3,22 @@ exports.handler = (event) => {
   try {
     const requestBody = JSON.parse(event.body);
 
-    if (requestBody.options.length === 4) {
+    if (requestBody.roll) {
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(requestBody.deathOption),
+      };
+      return response;
+    } else {
       const endGame = Math.random() < 0.04; // 4% chance of true
-      requestBody.endGame = endGame;
+      requestBody.roll = endGame;
+      const response = {
+        statusCode: 200,
+        body: JSON.stringify(requestBody),
+      };
+
+      return response;
     }
-
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify(requestBody),
-    };
-
-    return response;
   } catch (error) {
     console.error('Error appending endGame property to requestBody', error);
     return {
